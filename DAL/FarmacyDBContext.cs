794888fbfd3d83fb3afbbfcad775fbc026eb91d0
Rukuretia <rukuretio@gmail.com>
2025-06-01 18:53:32 +0200

    Migrations

---
+++ DAL/FarmacyDBContext.cs
@@ -0,0 +1,25 @@
+﻿using apbd11_cw5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apbd11_cw5.DAL;
+
+public class FarmacyDBContext : DbContext
+{
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+
+    protected FarmacyDBContext()
+    {
+        
+    }
+
+    public FarmacyDBContext(DbContextOptions<FarmacyDBContext> options) : base(options)
+    {
+        
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using apbd11_cw5.DAL;
+using Microsoft.EntityFrameworkCore;
+
 namespace apbd11_cw5;
 
 public class Program
@@ -6,9 +9,14 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
         // Add services to the container.
         builder.Services.AddAuthorization();
         builder.Services.AddControllers();
+        builder.Services.AddDbContext<FarmacyDBContext>(opt =>
+        {
+            opt.UseSqlServer(connectionString);
+        });
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
